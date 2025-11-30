00746c3f657ff48de8d08a86a8f255adeb5ab81c
Maxmudbek <muzaffarovmahmudbek@gmail.com>
2025-11-30 14:49:39 +0500

    feat: add server configuration to Vite for development

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -15,6 +15,10 @@ export default defineConfig({
 		}),
 		devtoolsJson()
 	],
+	server: {
+		host: '0.0.0.0', 
+        port: 5173
+	},
 	test: {
 		expect: { requireAssertions: true },
 		projects: [
